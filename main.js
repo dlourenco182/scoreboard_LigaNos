5d9b2624ff0d511e0f6f6364ce2d3934474ec6ed
David Luciano Gonçalves Lourenço <david.lourenco11@gmail.com>
2018-02-05 22:11:57 +0000

    Added Goal Feature
    Now we have goals! Just press the button Goal! and see who scored!

--- main.js
+++ main.js
@@ -1,8 +1,27 @@
 $(document).ready(function () {
-    $("#goal").click(function () {
-        $("#eq_slb").css('zIndex', '2');
-        $("#eq_slb").animate({
-            width: '100%'
-        });
+    /*
+        Not too good looking right now but it will evolve. Right now the priority is functionality!
+            $("#goal").click(function () {
+            $("#eq_slb").css('zIndex', '2');
+            $("#eq_slb").animate({
+                width: '100%'
+            });
+        });*/
+    $(".goal").click(function () {
+
+        var randomNumber = Math.floor(Math.random() * 2) + 1;
+        if (randomNumber === 1) {
+            var element = document.getElementById('score_home');
+            var goal_home = parseInt(element.innerHTML);
+            goal_home++;
+            element.innerHTML = goal_home;
+        } else {
+            var elements = document.getElementById('score_away');
+            var goal_away = parseInt(elements.innerHTML);
+            goal_away++;
+            elements.innerHTML = goal_away;
+        }
+
     });
+
 });
